60882deac37b9489b8d91b1fdcc200de58274777
Preethi <preethis@thoughtworks.com>
2015-07-04 22:53:48 +0530

    Preethi | Displaying kural

--- src/com/android/tirukkural/MainActivity.java
+++ src/com/android/tirukkural/MainActivity.java
@@ -1,51 +1,75 @@
 package com.android.tirukkural;
 
-import android.annotation.TargetApi;
-import android.app.ListActivity;
-import android.database.Cursor;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.SimpleCursorAdapter;
+import android.annotation.*;
+import android.app.*;
+import android.database.*;
+import android.graphics.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+import java.io.*;
+import java.sql.SQLException;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class MainActivity extends ListActivity {
 
-    DatabaseOpenHelper mDbHelper;
+    DatabaseHelper mDbHelper;
     SimpleCursorAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_main);
-	mDbHelper = new DatabaseOpenHelper(this);
-	Cursor kuralCursor = readKural();
-	mAdapter = new SimpleCursorAdapter(this, R.layout.list_layout, kuralCursor, new String[]{DatabaseOpenHelper.LINE_1, DatabaseOpenHelper.LINE_2},
-		new int[] {
-		R.id.line1, R.id.line2 }, 0);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDbHelper = new DatabaseHelper(this);
+        try {
+            mDbHelper.createDataBase();
+        } catch (IOException ioe) {
+            throw new Error("Unable to create database");
+        }
 
-	setListAdapter(mAdapter);
+        try {
+            mDbHelper.openDataBase();
+
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        Cursor kuralCursor = readKural();
+        mAdapter = new SimpleCursorAdapter(this, R.layout.list_layout, kuralCursor, new String[]{"_id", "line1", "line2"},
+                new int[]{0, R.id.line1, R.id.line2}, 0);
+
+        mAdapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
+            Typeface tamilFont = Typeface.createFromAsset(getAssets(), "fonts/Bamini.ttf");
+
+            @Override
+            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
+                TextView textView = (TextView) view;
+                textView.setTypeface(tamilFont);
+                return false;
+            }
+        });
+
+        setListAdapter(mAdapter);
 
     }
 
     private Cursor readKural() {
-	return mDbHelper.getWritableDatabase().query(DatabaseOpenHelper.TABLE_NAME, DatabaseOpenHelper.columns, null, new String[] {},
-		null, null, null);
+        return mDbHelper.getWritableDatabase().query("couplets", new String[]{"_id", "line1", "line2"}, null, new String[]{},
+                null, null, null);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-	getMenuInflater().inflate(R.menu.main, menu);
-	return true;
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-	int id = item.getItemId();
-	if (id == R.id.action_settings) {
-	    return true;
-	}
-	return super.onOptionsItemSelected(item);
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
